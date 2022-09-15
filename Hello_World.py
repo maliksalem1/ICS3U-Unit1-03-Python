a4f694550e88de7b3a29adfea266c8618d9636f8
Malik Salem <malik.salem@mths.ca>
2022-09-15 15:15:55 +0000

    code

---
+++ Hello_World.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: maliksalem1
+# Created on: Sept 2022
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
